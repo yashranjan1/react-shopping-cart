104ab2fbff891d46ea7c8b9cd41c69670c73d1a0
Yash <yash.ranjan25@gmail.com>
2024-01-07 18:30:36 +0530

    css animations, card template, quantity change added, still needs add to cart feature and cart page

--- react-shopping-cart/src/App.jsx
+++ react-shopping-cart/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import './App.css'
 import Products from './components/Products'
 import Cart from './components/Cart'
@@ -6,17 +6,41 @@ import Navbar from './components/Navbar'
 import { Routes, Route } from 'react-router-dom'
 
 function App() {
-    const [count, setCount] = useState(0)
+
+    const mouseRef = useRef(null)
+
+    useEffect(() => {
+        const updateMousePositionFunction = (event) => {
+            if (!mouseRef.current) return
+            const { clientX, clientY } = event;
+            mouseRef.current.style.setProperty('--x', `${clientX}px`)
+            mouseRef.current.style.setProperty('--y', `${clientY}px`)
+        }
+        window.addEventListener('mousemove', updateMousePositionFunction)
+
+        return () => {
+            window.removeEventListener('mousemove', updateMousePositionFunction)
+        }
+    }, []);
+    
+    const items = [
+        {
+            id: 1,
+            itemName: "Nike Air Force 1 '07",
+            price: "10,795.00",
+            itemImage: "nike-air-force-107.png"
+        }
+    ]
 
     return (
-        <>
+        <div ref={mouseRef} className='h-screen bg-[radial-gradient(circle_farthest-side_at_var(--x,_100px)_var(--y,_100px),_var(--color-secondary)_0%,_transparent_50%,_transparent_100%)] '>
             <Navbar />
             <Routes>        
-                <Route path="/"  element={<Products/>} />
-                <Route path="/products"  element={<Products />}/>
+                <Route path="/"  element={<Products items={ items }/>} />
+                <Route path="/products"  element={<Products items={ items }/>}/>
                 <Route path="/cart"  element={<Cart/>}/>
             </Routes>
-        </>
+        </div>
     )
 }
 
--- react-shopping-cart/src/components/Navbar/Navbar.jsx
+++ react-shopping-cart/src/components/Navbar/Navbar.jsx
@@ -2,12 +2,12 @@ import { Link } from "react-router-dom";
 
 const Navbar = () => {
     return ( 
-        <nav class="flex items-center justify-between flex-wrap bg-theme-color py-6 pr-6 drop-shadow-nav">
-            <div class="flex items-center flex-shrink-0 text-white mx-8 font-bold text-3xl rotate-[350deg] font-montserrat">
+        <nav className="flex items-center justify-between flex-wrap bg-theme-color py-6 pr-6 drop-shadow-nav">
+            <div className="flex items-center flex-shrink-0 text-white mx-8 font-bold text-3xl rotate-[350deg] font-montserrat">
                 SC
             </div>
-            <div class="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
-                <div class="text-sm lg:flex-grow">
+            <div className="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
+                <div className="text-sm lg:flex-grow">
                     <Link to='/products' className="block mt-4 lg:inline-block lg:mt-0 text-slate-50 hover:text-slate-300 mr-4 text-xl font-montserrat">Products</Link>
                     <Link to='/cart'className="block mt-4 lg:inline-block lg:mt-0 text-slate-50 hover:text-slate-300 mr-4 text-xl font-montserrat">Cart</Link>
                 </div>
